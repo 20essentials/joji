31dcbb7cb0efa056fb78493ff5cf24665d9d118e
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:37:42 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JOJI';
+export const nameSongs = [
+	"AFTERTHOUGHT", /*1*/
+	"DIE FOR YOU", /*2*/
+	"GLIMPSE OF US", /*3*/
+	"LIKE YOU DO", /*4*/
+	"EW", /*5*/
+	"TICK TOCK", /*6*/
+	"PRETTY BOY", /*7*/
+	"MODUS", /*8*/
+	"YUKON", /*9*/
+	"777", /*10*/
+	"MR. HOLLYWOOD", /*11*/
+	"NITROUS", /*12*/
+	"BEFORE THE DAY IS OVER", /*13*/
+	"UPGRADE", /*14*/
+	"NORMAL PEOPLE", /*15*/
+	"HIGH HOPES", /*16*/
+	"FEELING LIKE THE END", /*17*/
+	"NIGHT RIDER", /*18*/
+	"1AM FREESTYLE", /*19*/
+	"DISSOLVE", /*20*/
+]
